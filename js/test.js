a01ed0c687ca879dcfaee15a745c2e97c4740677
loveFluffy <blissLove8@gmail.com>
2023-12-17 09:27:17 -0500

    看到对象基础知识部分了

--- js/test.js
+++ js/test.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// =========================== JavaScript 基础知识 =========================== //
+
 // alert("I'm Javascript!");
 // alert("I'm Javascript too!");
 
@@ -306,32 +308,137 @@
 // }
 // alert(min(2, 3));
 
+// let min = (a, b) => a < b ? a : b;
+
+// function ask(question, yes, no) {
+// 	if (confirm(question)) yes();
+// 	else no();
+// }
+// ask(
+// 	"Do you agree?",
+// 	() => alert("You agreed."),
+// 	() => alert("You canceled the execution.")
+// );
 
 
 
 
+// =========================== 代码质量 =========================== //
+
+// function pow(x, n) {
+// 	let result = 1;
+// 	for (let i = 0; i < n; i++) {
+// 		result *= x;
+// 	}
+// 	return result;
+// }
+
+// let x = prompt("x?", ''), n = prompt("n?", '');
+// if (n <= 0) {
+// 	alert(`Power ${n} is not supported, please enter an integer number greater than zero`);
+// }
+// else {
+// 	alert(pow(x, n));
+// }
 
 
+// =========================== Object（对象）：基础知识 =========================== //
 
+// let obj = {};
+// obj._proto_ = 5;
+// alert(obj._proto_);
 
+// let obj = {
+// 	name: "John",
+// 	age: 9,
+// 	key: 1,
+// };
+// alert("name" in obj);
+// alert(obj.name);
+// alert("Name" in obj);
+// alert(obj.Name); //访问不存在的属性，只会返回undefined，不会报错！
+// for (let key in obj) {
+// 	alert(key);
+// 	alert(obj[key]);
+// 	alert(obj.key);
+// 	alert(obj["key"]);
+// }
+// delete obj.key;
+// alert("key" in obj);
 
+// 按下面的要求写代码，一条对应一行代码：
+//     创建一个空的对象 user。
+//     为这个对象增加一个属性，键是 name，值是 John。
+//     再增加一个属性，键是 surname，值是 Smith。
+//     把键为 name 的属性的值改成 Pete。
+//     删除这个对象中键为 name 的属性。
+// let user = {};
+// user.name = "John";
+// user.surname = "Smith";
+// user.name = "Pete";
+// delete user.name;
 
+// 写一个 isEmpty(obj) 函数，当对象没有属性的时候返回 true，否则返回 false。应该像这样：
+// function isEmpty(obj) {
+// 	for (let key in obj) {
+// 		return false;
+// 	}
+// 	return true;
+// }
+// let schedule = {};
+// alert(isEmpty(schedule)); // true
+// schedule["8:30"] = "get up";
+// alert(isEmpty(schedule)); // false
 
+// 我们有一个保存着团队成员工资的对象：
+// 写一段代码求出我们的工资总和，将计算结果保存到变量 sum。从所给的信息来看，结果应该是 390。如果 salaries 是一个空对象，那结果就为 0。
+// let salaries = {
+// 	John: 100,
+// 	Ann: 160,
+// 	Pete: 130
+// }
+// let sum = 0;
+// for (let key in salaries) {
+// 	sum += salaries[key];
+// }
+// alert(sum);
 
+// 创建一个 multiplyNumeric(obj) 函数，把 obj 所有的数值属性值都乘以 2。例如：
+// 注意 multiplyNumeric 函数不需要返回任何值，它应该就地修改对象。 P.S.用 typeof 检查值类型。
+// function multiplyNumeric(obj) {
+// 	for (let key in obj) {
+// 		if (typeof obj[key] == "number") {
+// 			obj[key] *= 2;
+// 		}
+// 	}
+// }
+// 在调用之前
+// let menu = {
+// 	width: 200,
+// 	height: 300,
+// 	title: "My menu"
+// };
+// multiplyNumeric(menu);
+// 调用函数之后
+// menu = {
+// 	width: 400,
+// 	height: 600,
+// 	title: "My menu"
+// };
+// alert(menu.width);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//Object.assign() 合并对象
+// let User = {
+// 	name: "John",
+// }
+// let permission1 = {
+// 	canView: true,
+// }
+// let permission2 = {
+// 	canEdit: true,
+// }
+// Object.assign(User, permission1, permission2);
+// for (let key in User) {
+// 	alert(User[key]);
+// }
 
